c5471ea6fcde632bb5b831697cd7c1cdc6d6e9ad
Nikhil Juturu <nikhil.juturu@lochbridge.com>
2015-11-08 19:48:07 -0500

    submission

--- plot4.R
+++ plot4.R
@@ -21,8 +21,28 @@ electric_data$Sub_metering_1 <- as.numeric(electric_data$Sub_metering_1)
 electric_data$Sub_metering_2 <- as.numeric(electric_data$Sub_metering_2)
 electric_data$Sub_metering_3 <- as.numeric(electric_data$Sub_metering_3)
 
+#Converting Voltage to numeric
+electric_data$Voltage <- as.numeric(electric_data$Voltage)
+
+
 #Open a png device
-png('plot3.png',width = 480, height = 480)
+png('plot4.png',width = 480, height = 480)
+
+#Creating Matrix
+par(mfrow = c(2, 2)) 
+
+#Plotting the elements
+plot(datetime, electric_data$Global_active_power, 
+     type="l", 
+     xlab="", 
+     ylab="Global Active Power", 
+     cex=0.2)
+
+plot(datetime, 
+     electric_data$Voltage, 
+     type="l", 
+     xlab="datetime", 
+     ylab="Voltage")
 
 #plotting the histogram
 plot(datetime, electric_data$Sub_metering_1, 
@@ -35,8 +55,13 @@ plot(datetime, electric_data$Sub_metering_1,
 lines(datetime, electric_data$Sub_metering_2, type = 'l', col='red')
 lines(datetime, electric_data$Sub_metering_3, type = 'l', col='blue')
 
-
 #Adding legend to the top right for all submetering
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),  lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+plot(datetime, electric_data$Global_reactive_power, 
+     type="l", 
+     xlab="datetime", 
+     ylab="Global_reactive_power")
+
 
 dev.off()
